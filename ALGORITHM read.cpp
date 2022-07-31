71a9725f9caf209285e65153a64f5df3281396f3
HAMMAKCH <hamakch12@gmail.com>
2022-07-31 18:16:24 +0200

    first commit

---
+++ ALGORITHM read.cpp
@@ -0,0 +1,22 @@
+ALGORITHM read
+VAR 
+str : STRING[];
+i,j,k: INTEGER;
+begin
+write("give me a sentence");
+read(str);
+
+int j(string str)
+{
+    int j = 0;
+    for (int i=0; i<str.length(); i++)
+        if (j(str[i])) 
+            ++count;
+    return j;
+
+    str:= str.concat('.');
+
+    write("my new sentence is"  : str);
+    end 
+
+
